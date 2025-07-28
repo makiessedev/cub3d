184143b4d1fafbc5b4460756096a37e3b8153020
Makiese Morais <makiesse.dev@gmail.com>
2025-07-28 09:24:12 +0100

    dsadsa

--- libft/libft.h
+++ libft/libft.h
@@ -17,6 +17,10 @@
 # include <stdint.h>
 # include <stdio.h>
 
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
 typedef struct s_list
 {
 	void			*content;
@@ -66,5 +70,6 @@ void	ft_lstdelone(t_list *lst, void (*del)(void *));
 void	ft_lstclear(t_list **lst, void (*del)(void *));
 void	ft_lstiter(t_list *lst, void (*f)(void *));
 t_list	*ft_lstmap(t_list *lst,	void *(*f)(void *), void (*del)(void *));
+char	*get_next_line(int fd);
 
 #endif
