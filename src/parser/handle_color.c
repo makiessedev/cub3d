3128bd8a2cb7063e8c8d35c0fc847e5666215118
Zola Mbunga <zolambunga@gmail.com>
2025-10-10 09:47:32 +0100

    fix: validate colors

--- src/parser/handle_color.c
+++ src/parser/handle_color.c
@@ -6,7 +6,7 @@
 /*   By: mmorais <makiesse.dev@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/08 10:53:28 by mmorais           #+#    #+#             */
-/*   Updated: 2025/10/08 10:53:29 by mmorais          ###   ########.fr       */
+/*   Updated: 2025/10/10 09:47:02 by mmorais          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,19 +59,19 @@ static void	set_colors(t_cub *cub, char *colors_raw, char **colors_ref)
 
 	i = 0;
 	colors = ft_split(colors_raw, ',');
-	if (ft_count_matrix(colors) > 3)
+	if (ft_count_matrix(colors) != 3)
 	{
 		ft_free_matrix(colors);
-		print_error_and_exit(cub, "Invalid color many arguments");
+		print_error_and_exit(cub, "Invalid color RGB");
 	}
 	while (i < 3)
 	{
 		j = 0;
-		while (j < 3)
+		while (j < (int)ft_strlen(colors[i]))
 		{
 			if (ft_isprint(colors[i][j]))
 			{
-				if (!ft_isdigit(colors[i][j]))
+        if (!ft_isdigit(colors[i][j]))
 				{
 					ft_free_matrix(colors);
 					print_error_and_exit(cub, "Invalid digit color");
