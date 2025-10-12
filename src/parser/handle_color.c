c7cacc713271d9c1eb69bbce0863448fe64afc95
Zola Mbunga <zolambunga@gmail.com>
2025-10-13 00:45:05 +0100

    remove las bug

--- src/parser/handle_color.c
+++ src/parser/handle_color.c
@@ -6,91 +6,103 @@
 /*   By: mmorais <makiesse.dev@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/08 10:53:28 by mmorais           #+#    #+#             */
-/*   Updated: 2025/10/12 23:24:08 by mmorais          ###   ########.fr       */
+/*   Updated: 2025/10/13 00:43:40 by mmorais          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/header.h"
 
-static bool	set_colors(char *colors_raw, char **colors_ref);
+static char	*process_color_line(char **chuncks);
+static bool	validate_and_set_colors(t_cub *cub, char *color, char **colors_ref,
+				char *to_free);
+static bool	parse_color_values(char *colors_raw, char **colors_ref);
 
 void	handle_color(t_cub *cub, char **chuncks, char **colors_ref,
 		char *to_free)
 {
-	int			elements_len;
-	const char	*empty = "\t ";
-	char		*color;
-	char		*temp;
-	int			i;
+	int		elements_len;
+	char	*color;
 
 	elements_len = ft_count_matrix(chuncks);
-	if (elements_len >= 2 && elements_len <= 6)
-	{
-		color = ft_strdup(chuncks[1]);
-		if (elements_len > 2)
-		{
-			i = 2;
-			while (chuncks[i])
-			{
-				temp = ft_strdup(color);
-				free(color);
-				color = ft_strjoin(temp, chuncks[i]);
-				free(temp);
-				i++;
-			}
-			temp = strdup(color);
-			free(color);
-			color = ft_remove_chars(temp, empty);
-			free(temp);
-		}
-		if (set_colors(color, colors_ref) == false)
-		{
-			free(color);
-			free(to_free);
-			ft_free_matrix(chuncks);
-			print_error_and_exit(cub, "Invalid digit color");
-		}
-		free(color);
-	}
-	else
+	if (elements_len < 2 || elements_len > 6)
 	{
 		free(to_free);
 		ft_free_matrix(chuncks);
 		print_error_and_exit(cub, "Invalid arguments of colors");
 	}
+	color = process_color_line(chuncks);
+	if (validate_and_set_colors(cub, color, colors_ref, to_free) == false)
+	{
+		free(color);
+		free(to_free);
+		ft_free_matrix(chuncks);
+		print_error_and_exit(cub, "Invalid digit color");
+	}
+	free(color);
 }
 
-static bool	set_colors(char *colors_raw, char **colors_ref)
+static char	*process_color_line(char **chuncks)
 {
+	char		*color;
+	char		*temp;
+	const char	*empty = "\t ";
+	int			i;
+
+	color = ft_strdup(chuncks[1]);
+	i = 2;
+	while (chuncks[i])
+	{
+		temp = ft_strdup(color);
+		free(color);
+		color = ft_strjoin(temp, chuncks[i]);
+		free(temp);
+		i++;
+	}
+	temp = ft_strdup(color);
+	free(color);
+	color = ft_remove_chars(temp, empty);
+	free(temp);
+	return (color);
+}
+
+static bool	validate_and_set_colors(t_cub *cub, char *color, char **colors_ref,
+		char *to_free)
+{
+	if (parse_color_values(color, colors_ref) == false)
+	{
+		free(color);
+		free(to_free);
+		print_error_and_exit(cub, "Invalid digit color");
+		return (false);
+	}
+	return (true);
+}
+
+static bool	parse_color_values(char *colors_raw, char **colors_ref)
+{
+	char	**colors;
 	int		i;
 	int		j;
-	char	**colors;
 	int		digit;
 
-	i = 0;
 	colors = ft_split(colors_raw, ',');
 	if (ft_count_matrix(colors) != 3)
 		return (ft_free_matrix(colors), false);
+	i = 0;
 	while (i < 3)
 	{
 		j = 0;
-		while (j < (int)ft_strlen(colors[i]))
+		while (colors[i][j])
 		{
-			if (ft_isprint(colors[i][j]))
-			{
-				if (!ft_isdigit(colors[i][j]))
-					return (ft_free_matrix(colors), false);
-			}
+			if (!ft_isdigit(colors[i][j]))
+				return (ft_free_matrix(colors), false);
 			j++;
 		}
 		digit = ft_atoi(colors[i]);
-		if (digit > 255 || digit < 0)
-			return (ft_free_matrix(colors), false);
-		if (colors_ref[i])
+		if (digit < 0 || digit > 255 || colors_ref[i])
 			return (ft_free_matrix(colors), false);
 		colors_ref[i] = ft_strdup(colors[i]);
 		i++;
 	}
-	ft_free_matrix(colors);
-	return (true);
+	return (ft_free_matrix(colors), true);
 }
