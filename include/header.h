c78a91cf03f51982b4371748478bd2f32284da0d
Makiese Morais <makiesse.dev@gmail.com>
2025-10-13 05:41:32 +0100

    fix norminette on header file

--- include/header.h
+++ include/header.h
@@ -10,8 +10,8 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef CUB_H
-# define CUB_H
+#ifndef HEADER_H
+# define HEADER_H
 # include "../libft/libft.h"
 # include "../mlx/mlx.h"
 # include <errno.h>
